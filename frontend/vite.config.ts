495eb19c4ca780ef7b80565d98e097ce01b6bfa5
jiad-saegus <jiad.abdul@saegus.com>
2024-02-20 22:02:35 +0100

    Docker file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ["**/*.xlsx"], // xlsx file should be treated as assets
+
   plugins: [react()],
-})
+
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
+});
+
